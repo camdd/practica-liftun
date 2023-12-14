5b9525f2c06d7d192d99e58b5ec1471b5dafc2c4
Cam <camila.dambrosio95@gmail.com>
2023-12-14 18:12:28 -0300

    Escala switches

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,30 @@
-import ActionButton from '../ActionButton'
-import ActionSelector from '../ActionSelector'
-import Calendar from '../Calendar'
-import OptionSwitch from '../OptionSwitch'
-
+import ActionButton from "../ActionButton";
+import ActionSelector from "../ActionSelector";
+import Calendar from "../Calendar";
+import OptionSwitch from "../OptionSwitch";
+import { TextField } from "@radix-ui/themes";
 
 const Card = () => {
- return(
-    <div className='container'>
-    <ActionButton/>
-    <ActionSelector/>
-    <Calendar/>
-    <OptionSwitch/>
-    </div>
- )
-}
+  return (
+    <div className="container">
+      <TextField.Input placeholder="Nombre de la acción" />
+      <div className="calendar-container">
+        <div className="calendar-icon"></div>
+        <Calendar />
+      </div>
+      <div className="switch-question">
+        <OptionSwitch />
+      </div>
 
-export default Card
+      <div className="action-type"></div>
+      <ActionSelector />
+
+      <div className="buttons">
+        <ActionButton />
+        <ActionButton />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/OptionSwitch/OptionSwitch.jsx
+++ src/components/OptionSwitch/OptionSwitch.jsx
@@ -1,21 +1,31 @@
 import * as Switch from "@radix-ui/react-switch";
-import './OptionSwitch.css'
+import "./OptionSwitch.css";
 
 const OptionSwitch = () => {
   return (
-    <form>
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <label className="Label" htmlFor="airplane-mode" style={{ paddingRight: 15 }}>
-        No
-      </label>
-      <Switch.Root className="SwitchRoot" id="airplane-mode">
-        <Switch.Thumb className="SwitchThumb" />
-      </Switch.Root>
-      <label className="Label" htmlFor="airplane-mode" style={{ paddingLeft: 15 }}>
-        Si
-      </label>
+    <div className="switch-container">
+      <form>
+        <div className="switch-yes-no">
+          <label
+            className="Label"
+            htmlFor="airplane-mode"
+            style={{ paddingRight: 15 }}
+          >
+            No
+          </label>
+          <Switch.Root className="SwitchRoot" id="airplane-mode">
+            <Switch.Thumb className="SwitchThumb" />
+          </Switch.Root>
+          <label
+            className="Label"
+            htmlFor="airplane-mode"
+            style={{ paddingLeft: 15 }}
+          >
+            Si
+          </label>
+        </div>
+      </form>
     </div>
-  </form>
   );
 };
 
